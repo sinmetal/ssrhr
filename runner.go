004ceb7e43d893e404f3231759d03350ec44d322
sinmetal <metal.tie@gmail.com>
2019-09-01 14:04:35 +0900

    row numberが分かりやすいようにTraceNameに入れた

--- runner.go
+++ runner.go
@@ -9,16 +9,15 @@ import (
 func goGetSmallSize(sss SmallSizeStore, goroutine int, rowNumber int, endCh chan<- error) {
 	go func() {
 		for {
-			fmt.Printf("rowNumber=%d\n", rowNumber)
 			var wg sync.WaitGroup
 			for i := 0; i < goroutine; i++ {
 				i := i
 				wg.Add(1)
 				go func(i int) {
 					defer wg.Done()
-					fmt.Printf("goroutine=%d,rowNumber=%d\n", i, rowNumber)
+
 					ctx := context.Background()
-					ctx, span := startSpan(ctx, "/go/goGetSmallSize")
+					ctx, span := startSpan(ctx, fmt.Sprintf("/go/goGetSmallSize/%d", rowNumber))
 					defer span.End()
 
 					_, err := sss.Get(ctx, fmt.Sprintf("small%v", rowNumber))
