9b8e2a7bd24f2853753ed69362c449ce48610b70
sinmetal <metal.tie@gmail.com>
2019-09-01 13:48:31 +0900

    add debug log

--- runner.go
+++ runner.go
@@ -9,13 +9,14 @@ import (
 func goGetSmallSize(sss SmallSizeStore, goroutine int, rowNumber int, endCh chan<- error) {
 	go func() {
 		for {
+			fmt.Printf("rowNumber=%d\n", rowNumber)
 			var wg sync.WaitGroup
 			for i := 0; i < goroutine; i++ {
 				i := i
 				wg.Add(1)
 				go func(i int) {
 					defer wg.Done()
-
+					fmt.Printf("goroutine=%d,rowNumber=%d\n", i, rowNumber)
 					ctx := context.Background()
 					ctx, span := startSpan(ctx, "/go/goGetSmallSize")
 					defer span.End()
